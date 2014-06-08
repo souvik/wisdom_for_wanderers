b8fd3ee64bbdcb73df9c09f886664ee647b6f2c4
Souvik Kumar Khan <souvik.khan@ymail.com>
2014-06-07 20:58:39 -0500

    Added a page to list the posts.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,29 @@
+function equalizeHeights(items) {
+    var maxHeight = 0;
+    items.each(function() {
+        if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+    });
+    items.each(function() { 
+        $(this).css('min-height',(maxHeight+10)+"px");
+    });
+}
+
+function formatPostListings() {
+    var posts = $('body.posts div.posts div.thumbnail');
+    posts.each(function() {
+        $(this).css('min-height',"0px");
+    });
+    if( $(window).width() >= 992 ) {
+        for(var i = 0; i < posts.length; i+=3) {
+            equalizeHeights( posts.slice(i, i+3) );
+        }
+    } else if ( $(window).width() >= 768 ) {
+        for(var i = 0; i < posts.length; i+=2) {
+            equalizeHeights( posts.slice(i, i+2) );
+        }
+    }g
+}
+
 $(document).ready(function() {
     // if they click on a boxed image, enlarge it
     $('div.img-box a').click(function(event) {
@@ -14,4 +40,7 @@ $(document).ready(function() {
     $('body').on('click', '#lightbox', function(event) {
         $('#lightbox').remove();
     });
+
+    $(window).on('load', formatPostListings);
+    $(window).on('resize', formatPostListings);
 });
